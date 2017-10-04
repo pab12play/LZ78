45fe5b697b88b9c56a56915fd4e2e7e68c5c7dab
Pablo Pineda <pablo12.izq@gmail.com>
2017-10-04 17:20:06 -0600

    LZ78 completed

---
+++ LZ78/Encoded.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LZ78
+{
+    class Encoded
+    {
+        int index;
+        char character;
+
+        public Encoded(int index, char character)
+        {
+            this.Index = index;
+            this.Character = character;
+        }
+
+        public int Index { get => index; set => index = value; }
+        public char Character { get => character; set => character = value; }
+    }
+}
--- LZ78/Program.cs
+++ LZ78/Program.cs
@@ -10,6 +10,79 @@ namespace LZ78
     {
         static void Main(string[] args)
         {
+            //string text = "DAD-DADA-DADDY-DADO";
+            string text = args[0];
+            Dictionary<string, Encoded> dictionary = new Dictionary<string, Encoded>();
+            for(int index = 0; index < text.Length; index++)
+            {
+                if (dictionary.ContainsKey(text[index].ToString()))
+                {
+                    string word = text[index].ToString();
+                    string nextChar = "";
+                    while (dictionary.ContainsKey(word+nextChar))
+                    {
+                        if (nextCharExist(text, index))
+                        {
+                            word = word + nextChar;
+                            nextChar = getNextChar(text, ref index);
+                        }
+                        else
+                        {
+                            break;
+                        }
+                    }
+                    dictionary.Add(word+nextChar, new Encoded(getIndex(dictionary,word), text[index]));
+                }
+                else
+                {
+                    dictionary.Add(text[index].ToString(), new Encoded(0, text[index]));
+                }
+            }
+            print(dictionary);
+            Console.ReadLine();
+        }
+
+        static void print(Dictionary<string, Encoded> dictionary)
+        {
+            Console.Clear();
+            int i = 1;
+            Console.WriteLine("Salida\tIndex\tCadena");
+            foreach (KeyValuePair<string, Encoded> pair in dictionary)
+            {
+                Console.WriteLine("<" + pair.Value.Index + ",C(" + pair.Value.Character + ")>\t" + i + "\t" + pair.Key);
+                i++;
+            }
+        }
+
+        static int getIndex(Dictionary<string, Encoded> dictionary,string word)
+        {
+            int i = 1;
+            foreach(KeyValuePair<string,Encoded> pair in dictionary)
+            {
+                if (pair.Key.Equals(word))
+                {
+                    return i;
+                }
+                i++;
+            }
+            return 0;
+        }
+
+        static bool nextCharExist(string text, int index)
+        {
+            if (index < text.Length)
+            {
+                return true;
+            }
+            else
+            {
+                return false;
+            }
+        }
+
+        static string getNextChar(string text,ref int index)
+        {
+            return text[++index].ToString();
         }
     }
 }
